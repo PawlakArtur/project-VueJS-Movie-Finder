ecf449ab31f4e269683c0148211f934c67f3d373
Artur Pawlak <pawlak.artur90@gmail.com>
2016-10-09 21:30:33 +0200

    fix, cleaning app.js

--- build/scripts/vuejs/app.js
+++ build/scripts/vuejs/app.js
@@ -3,8 +3,7 @@ var vueApp = new Vue({
     data: {
         title: '',
         movieFound: [],
-        movies: [],
-
+        movies: []
     },
     methods: {
         submitTitle: function() {
@@ -58,78 +57,4 @@ var vueApp = new Vue({
             });
         }
     }
-});
-
-
-Vue.directive('collapse', {
-    bind: function () {
-        console.log("pierwsze uruchomienie");
-    },
-    update: function (test, oldtest) {
-        console.log(oldtest + " wow " + test);
-    }
-});
-var vueApp2 = new Vue({
-    el: '#vueApp2',
-    data: {
-        todos: [],
-        currTodo: "",
-        test: "test"
-    },
-    computed: {
-        uniqueImportance:function(){
-            importanceArray = [];
-            for (var i = 0; i < this.todos.length; i++) {
-                if(importanceArray.indexOf(this.todos[i].importance) === -1){
-                    importanceArray.push(this.todos[i].importance);
-                }
-            }
-            return importanceArray;
-        }
-    },
-    methods: {
-        addTodo: function(){
-            var tempImportance = this.newImportance;
-            if(this.newImportance === "" || !this.newImportance) {
-                tempImportance = "low"
-            }
-            this.todos.push({name:this.currTodo,editing:false,complete:false,importance:tempImportance});
-            this.currTodo = "";
-            this.newImportance = "";
-        },
-        remove: function(todo) {
-            var index = this.todos.indexOf(todo);
-            this.todos.splice(index,1);
-        },
-        edit: function(todo) {
-            var index = this.todos.indexOf(todo);
-            this.todos[index].editing = true;
-        },
-        save: function(todo) {
-            var index = this.todos.indexOf(todo);
-            this.todos[index].editing = false;
-        },
-        changeCheck: function(todo){
-            if(todo.complete){
-                todo.complete = false;
-            } else {
-                todo.complete = true;
-            }
-        },
-        submit: function(){
-            this.$http.post('/todos', this.todos).then(function(response) {
-                console.log(response.body)
-            }, function(response) {
-                console.log("errors!")
-            });
-        }
-    },
-    // created: function(){
-    //     this.$http.get('http://www.omdbapi.com/?t=harry').then(function(response) {
-    //         console.log(response.body);
-    //         var data = JSON.parse(response.body);
-    //     }, function(response) {
-    //         console.log("errors!")
-    //     });
-    // }
 });
